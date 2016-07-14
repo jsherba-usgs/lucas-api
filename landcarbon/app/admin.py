c7b4b19db343bf80bfe807eddac80cab20d1a29c
Brian Galey <bgaley@berkeley.edu>
2016-07-14 16:58:19 -0700

    Add admin list displays

--- landcarbon/app/admin.py
+++ landcarbon/app/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.RasterSeries)
-admin.site.register(models.RasterStore)
-admin.site.register(models.Tag)
+_models = (
+    models.RasterSeries,
+    models.RasterStore,
+    models.Tag,
+)
+
+for _model in _models:
+    admin.site.register(_model)
+
+
+@admin.register(models.Project)
+class SyncroSimAdmin(admin.ModelAdmin):
+    list_display = ('project', 'name', 'ssim')
+
+
+@admin.register(models.Scenario)
+class ScenarioAdmin(admin.ModelAdmin):
+    date_hierarchy = 'last_modified'
+    list_display = ('scenario', 'name', 'project', 'author')
+
+
+@admin.register(models.SyncroSim)
+class SyncroSimAdmin(admin.ModelAdmin):
+    list_display = ('db', 'id')
