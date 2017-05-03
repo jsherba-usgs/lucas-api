d734856e84de23cc3543b03e2a6be85d5d83cf70
jsherba-usgs <jsherba@usgs.gov>
2017-05-03 10:04:05 -0700

    update to raster_upload

--- connect_rasterstore_rasterseries.py
+++ connect_rasterstore_rasterseries.py
@@ -2,12 +2,14 @@ import sqlite3
 import pyspatialite.dbapi2 as db
 from ssim_api.ssim_query_functions import query_spatial_files_stateclass, query_projects, project_summary
 
-def stateclass_paths(sqlite_file):
+scenario_id = (1120,1201,1202,1203)
+iteration = range(1, 11)
+timestep=range(2001,2102)
+
+def stateclass_paths(sqlite_file, scenario_id, iteration, timestep):
 #Collect state_class paths
-	scenario_id = (6370,)
-	iteration = range(1, 21)
-	timestep=range(2001,2062)
-	stateclass_paths = query_spatial_files_stateclass(sqlite_file, scenario_id=None, iteration=None, timestep=timestep)
+	#scenario_id = (1120,1201,1202,1203)
+	stateclass_paths = query_spatial_files_stateclass(sqlite_file, scenario_id=scenario_id, iteration=iteration, timestep=timestep)
 	return stateclass_paths
 
 def spatialite_conn(landcarbondb):
@@ -20,19 +22,19 @@ def add_to_rasterstore(connection, stateclass_paths, series_ids=None):
 	c = connection[0]
 	conn = connection[1]
 	table_name = 'app_rasterstore'
-	width = 555
-	height = 398
-	srs ='PROJCS[\"NAD83 / UTM zone 4N\",GEOGCS[\"NAD83\",DATUM[\"North_American_Datum_1983\",SPHEROID[\"GRS 1980\",6378137,298.2572221010002,AUTHORITY[\"EPSG\",\"7019\"]],AUTHORITY[\"EPSG\",\"6269\"]],PRIMEM[\"Greenwich\",0],UNIT[\"degree\",0.01745quit32925199433],AUTHORITY[\"EPSG\",\"4269\"]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",-159],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",0],UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],AUTHORITY[\"EPSG\",\"26904\"]]'
+	width = 730
+	height = 1233
+	srs ='PROJCS["North_America_Albers_Equal_Area_Conic",GEOGCS["GCS_North_American_1983",DATUM["North_American_Datum_1983",SPHEROID["GRS_1980",6378137,298.257222101]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["False_Easting",0],PARAMETER["False_Northing",0],PARAMETER["longitude_of_center",-96],PARAMETER["Standard_Parallel_1",20],PARAMETER["Standard_Parallel_2",60],PARAMETER["latitude_of_center",40],UNIT["Meter",1],AUTHORITY["EPSG","102008"]]'
 	minval = '1.0'
-	maxval = '10.0'
+	maxval = '12.0'
 	nodata = -9999.0
 	xpixsize = '1000.0'
 	ypixsize = '1000.0'
 	name = 'NULL'
 	units = 'NULL'
-	geometry = 'POLYGON((-159.834808 18.790113,-154.573079 18.739518,-154.468468 22.324861,-159.854614 22.386072,-159.834808 18.790113))'
-	id = 1
-	image_path = 'Hawaii_Assessment_Final_GIF/'
+	geometry = 'POLYGON((-956.424520505 43.5754794945,-125.260128026 43.5754794945,-125.260128026 874.739871974,-956.424520505 874.739871974,-956.424520505 43.5754794945))'
+	id = int(get_max_rasterstore_id(connection)[0][0]) + 1
+	image_path = 'California_Climate_Assessment_ Model_v3.0.37/'
 
 	for index, row in stateclass_paths.iterrows():
 	    scenario = row['Scenario']
@@ -52,11 +54,16 @@ def add_to_rasterstore(connection, stateclass_paths, series_ids=None):
 	    id +=1
 	conn.close()
 
-def summary(sqlite_file):
-	projects = query_projects(sqlite_file)
-	project_id = projects["ProjectID"].iloc[0]
-	project_id =(project_id,)
-	projectsummary = project_summary(sqlite_file, project=project_id)[0]
+def summary(sqlite_file, project_id = None):
+	if project_id:
+		projectsummary = project_summary(sqlite_file, project=project_id)[0]
+	else: 
+		print(sqlite_file)
+		projects = query_projects(sqlite_file)
+		project_id = projects["ProjectID"].iloc[0]
+		project_id =(project_id,)
+		projectsummary = project_summary(sqlite_file, project=project_id)[0]
+
 	projectsummary = projectsummary[projectsummary.RunStatus == 3]
 	return projectsummary
 
@@ -66,39 +73,61 @@ def sqlite_conn(landcarbondb):
 	c = conn.cursor()
 	return c, conn
 
-def add_to_rasterseries(projectsummary, connection):
+def get_max_rasterseries_id(connection):
+	c = connection[0]
+	table_name = 'app_rasterseries'
+	c.execute("SELECT MAX(id) FROM `app_rasterseries`")
+	max_id = c.fetchall()
+	
+	return max_id
+
+
+def get_max_rasterstore_id(connection):
+	c = connection[0]
+	table_name = 'app_rasterseries'
+	c.execute("SELECT MAX(id) FROM `app_rasterstore`")
+	max_id = c.fetchall()
+	
+	return max_id
+
+def add_to_rasterseries(projectsummary, connection, scenario_id):
 	c = connection[0]
 	conn = connection[1]
 	table_name = 'app_rasterseries'
-	id = 1
+	id = int(get_max_rasterseries_id(connection)[0][0]) + 1
+	print(id)
 	series_ids = {}
+
 	for index, row in projectsummary.iterrows():
 	    scenario = row['ScenarioID']
-	    iterations = range(row['MinimumIteration'], row["MaximumIteration"]+1)
-	    begin = str(row["MinimumTimestep"]) + "-01-01 00:00:00"
-	    end = str(row["MaximumTimestep"]) + "-01-01 00:00:00"
-	    series_ids[scenario]={}
-	    for iteration in iterations:
-			iteration = '{:0>4}'.format(iteration)
-			slug = 'scenario-'+str(scenario)+'-spatial-it'+iteration+'-sc'
-			c.execute("INSERT INTO `app_rasterseries` (`id`,`name`,`slug`,`begin`,`end`) VALUES (?,?,?,?,?)", (id, 'NULL', slug, begin, end))
-			conn.commit()
-			series_ids[scenario][iteration] = id
-			id += 1
+	    print(scenario)
+	    print(scenario_id)
+	    if int(scenario) in scenario_id:
+		    iterations = range(row['MinimumIteration'], row["MaximumIteration"]+1)
+		    begin = str(row["MinimumTimestep"]) + "-01-01 00:00:00"
+		    end = str(row["MaximumTimestep"]) + "-01-01 00:00:00"
+		    series_ids[scenario]={}
+		    for iteration in iterations:
+				iteration = '{:0>4}'.format(iteration)
+				slug = 'scenario-'+str(scenario)+'-spatial-it'+iteration+'-sc'
+				c.execute("INSERT INTO `app_rasterseries` (`id`,`name`,`slug`,`begin`,`end`) VALUES (?,?,?,?,?)", (id, 'NULL', slug, begin, end))
+				conn.commit()
+				series_ids[scenario][iteration] = id
+				id += 1
 		 
 	conn.close()
 	return series_ids
 
-sqlite_file = r"/media/sf_jts_2016_09_01_LandCarbon_CDI/Hawaii_Assessment_Final_GIF/Hawaii LandCarbon Assessment.ssim"
-landcarbondb = r'/home/ubuntu/projects/landcarbon-cdi/landcarbon.db'
-
-projectsummary = summary(sqlite_file)
-
+sqlite_file = r"/home/jsherba-pr/Projects/landcarbon-cdi/landcarbon/media/California_Climate_Assessment_Model_v3.0.37.ssim"
+landcarbondb = r"/home/jsherba-pr/Projects/landcarbon-cdi/landcarbon.db"
+print("start")
+projectsummary = summary(sqlite_file, project_id=(4008,))
+print(projectsummary)
 connection = sqlite_conn(landcarbondb)
 
-series_ids = add_to_rasterseries(projectsummary, connection)
+series_ids = add_to_rasterseries(projectsummary, connection, scenario_id)
 
-stateclass_paths = stateclass_paths(sqlite_file)
+stateclass_paths = stateclass_paths(sqlite_file, scenario_id, iteration, timestep)
 
 connection = spatialite_conn(landcarbondb)
 
